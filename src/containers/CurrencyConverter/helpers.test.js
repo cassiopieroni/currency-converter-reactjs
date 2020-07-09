e1e45b555d24100efb4566b35405e467bd3877a8
Cássio Pieroni <cassiopieroni@gmail.com>
2020-07-09 15:46:48 -0300

    Atualizando helpers.test.js

--- src/containers/CurrencyConverter/helpers.test.js
+++ src/containers/CurrencyConverter/helpers.test.js
@@ -1,58 +1,44 @@
-import * as helpers from '../helpers';
+import * as helpers from './helpers';
 
 describe('Teste do helper - validateForm(error, valueToConvert, currencies, selectedCurrencies)', () => {
     
+    const validParams = [ 
+        false, // error
+        10, // valueToConvert
+        [ { initial: "AFN", description: "Afghan Afghani"} ], //currencies
+        {currencyFrom: "BRL", currencyTo: "USD"} //selectedCurrencies
+    ];
+
     it("Deve garantir que se error === true, lance uma excessão", () => {
-        const params = [
-            true, 
-            10, 
-            [ { initial: "AFN", description: "Afghan Afghani"} ],
-            {currencyFrom: "BRL", currencyTo: "USD"}
-        ];
+        const invalidError = true;
+        const params = validParams.map( (p,i) => i === 0 ? invalidError : p );
 
         expect(() => helpers.validadeForm(...params)).toThrow(Error); 
     });
 
     it("Deve garantir que se valueToConvert === 0, lance uma excessão", () => {
-        const params = [
-            false, 
-            0, 
-            [ { initial: "AFN", description: "Afghan Afghani"} ],
-            {currencyFrom: "BRL", currencyTo: "USD"}
-        ];
+        const invalidValueToConvert = 0;
+        const params = validParams.map( (p,i) => i === 1 ? invalidValueToConvert : p );
 
         expect(() => helpers.validadeForm(...params)).toThrow(Error);
     });
 
     it("Deve garantir que se currencies.length === 0, lance uma excessão", () => {
-        const params = [
-            false, 
-            10, 
-            [],
-            {currencyFrom: "BRL", currencyTo: "USD"}
-        ];
+        const invalidCurrencies = [];
+        const params = validParams.map( (p,i) => i === 2 ? invalidCurrencies : p );
 
         expect(() => helpers.validadeForm(...params)).toThrow(Error);
     });
 
     it("Deve garantir que se selectedCurrencies não tiver props preenchidas, lance uma excessão", () => {
-        const params = [
-            false, 
-            10, 
-            [ { initial: "AFN", description: "Afghan Afghani"} ],
-            {currencyFrom: "", currencyTo: "USD"}
-        ];
+        const invalidSelectedCurrencies = {currencyFrom: "", currencyTo: "USD"};
+        const params = validParams.map( (p,i) => i === 0 ? invalidSelectedCurrencies : p );
 
         expect(() => helpers.validadeForm(...params)).toThrow(Error); 
     });
 
     it("Deve garantir que o formulário seja validado (true)", () => {
-        const params = [
-            false, 
-            "1",
-            [ { initial: "AFN", description: "Afghan Afghani"} ],
-            {currencyFrom: "BRL", currencyTo: "USD"}
-        ];
+        const params = validParams.slice();
 
         expect(helpers.validadeForm(...params)).toBe(true);
     });
@@ -63,9 +49,9 @@ describe('Teste do helper - getQuotation(quotations, selectedCurrencies, valueTo
 
     it("Deve garantir que a cotação de BRL: 6.072228 para USD: 1.136267 seja '0.19'", () => {
         const params = [
-            { BRL: 6.072228, USD: 1.136267 },
-            {currencyFrom: "BRL", currencyTo: "USD"},
-            1
+            { BRL: 6.072228, USD: 1.136267 }, //quotations
+            {currencyFrom: "BRL", currencyTo: "USD"}, //selectedCurrencies
+            1 //valueToConvert
         ]
 
         expect(helpers.getQuotation(...params)).toBe('0.19');
@@ -75,7 +61,7 @@ describe('Teste do helper - getQuotation(quotations, selectedCurrencies, valueTo
 
 describe('Teste do helper - createSerializedCurrencies(symbols)', () => {
 
-    it("Deve garantir que o objeto {AAA: 'nameA'} seja transformado em [{ initial: 'AAA', description: 'nameA'}]", () => {
+    it("Deve garantir que { AAA: 'nameA' } seja transformado em [{ initial: 'AAA', description: 'nameA'}]", () => {
         const symbols = { AAA: 'nameA' };
         const serialized = [{ initial: 'AAA', description: 'nameA'}];
         expect(helpers.createSerializedCurrencies(symbols)).toStrictEqual(serialized);
